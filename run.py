42fbd8fd3357a404a83097a490ee86537b16c88a
ArtA <pthung7102002@gmail.com>
2024-01-31 20:45:30 +0700

    update run.py

--- run.py
+++ run.py
@@ -1,17 +1,47 @@
-from omegaconf import DictConfig
-import shutil
-import hydra
+from rich import print, traceback
+from rich.prompt import Prompt
 
 from modules import DatasetGenerator
+from calibration import Calibrate
+from utils import load_config
+
+traceback.install()
 
 
-@hydra.main(config_path="./configs", config_name="run", version_base="1.3")
-def main(cfg: DictConfig):
-    # Remove the hydra outputs
-    shutil.rmtree("outputs")
+def main():
+    # Calibration
+    calibrator = Calibrate(**load_config("configs/calibration.yaml"))
 
-    # Generate data
-    DatasetGenerator(**cfg["data"], **cfg["labeling"]).run()
+    # Data generate
+    data_gen_cfg = load_config("configs/run.yaml")
+    data_gen = DatasetGenerator(**data_gen_cfg["data"], **data_gen_cfg["labeling"])
+
+    while True:
+        print()
+        print("[bold]Auto-Checkout Program[/]")
+        print()
+        print("1. Dataset generate")
+        print("2. Calibration")
+        print("3. Quit")
+        print()
+
+        choice = Prompt.ask("Enter", choices=["1", "2", "3"])
+        print()
+
+        if choice == "1":
+            print("Run Dataset Generator")
+            data_gen.run()
+        elif choice == "2":
+            print("Run Calibration")
+            print("Preparing")
+            calibrator.prepare(camera_id=int(Prompt.ask("Enter Camera ID")))
+            print("Calibrating")
+            calibrator.run()
+        elif choice == "3":
+            print("[red]Exit program[/]")
+            break
+        else:
+            continue
 
 
 if __name__ == "__main__":
