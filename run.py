a43d082fd16b218208eb9c6b1b92f65610b9a785
ArtA <pthung7102002@gmail.com>
2024-02-07 18:45:38 +0700

    update run.py

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
+from modules import TopEngine
 from modules.utils import load_config
-from modules import Camera
 
 
 def main():
-    top = Camera(**load_config("configs/camera.yaml"))
+    top_camera = TopEngine(
+        camera_ids=[0], camera_configs=load_config("configs/camera.yaml")
+    )
 
-    top.run()
+    top_camera.run()
 
 
 if __name__ == "__main__":
