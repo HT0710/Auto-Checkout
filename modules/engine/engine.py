18672838970513fcc8fc253a7e3952008db07ce0
ArtA <pthung7102002@gmail.com>
2024-03-05 12:07:48 +0700

    Update engine.py

--- modules/engine/engine.py
+++ modules/engine/engine.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from typing import List
 
+import numpy as np
 import cv2
 
 from ..utils import load_config
@@ -63,6 +64,8 @@ class CameraControler:
         After the loop exits, it releases all camera resources.
         """
 
+        Server.set("status", "STOP")
+
         stop = False
 
         # Main loop for capturing frames from cameras
@@ -70,6 +73,8 @@ class CameraControler:
             signal = Server.get("status")
 
             results = {"top": None, "side": []}
+
+            frames = []
 
             for key, value in self.engines.items():
                 engine = value["self"]
@@ -89,14 +94,27 @@ class CameraControler:
                         if key == "side":
                             results["side"].append(output["results"])
 
-                    # Display frame
-                    cv2.imshow(str(camera["self"].device_id), frame)
+                    frame = cv2.resize(frame, (0, 0), fx=0.7, fy=0.7)
 
-                    # Check for delay on each camera
-                    if not camera["self"].delay(camera["self"].wait):
-                        stop = True
+                    frames.append(frame)
 
-            if signal == "STOP":
+            review = np.concatenate(frames)
+
+            # Display frame
+            cv2.namedWindow("Review", cv2.WINDOW_GUI_NORMAL)
+            cv2.imshow("Review", review)
+
+            # Check for delay on each camera
+            if not all(
+                [
+                    camera["self"].delay(camera["self"].wait)
+                    for engine in self.engines.values()
+                    for camera in engine["cameras"]
+                ]
+            ):
+                stop = True
+
+            if signal != "SCAN":
                 continue
 
             total = results["top"]
@@ -117,10 +135,23 @@ class CameraControler:
             for key in conflict.keys():
                 products[key] += 1
 
+            if len(products) > total:
+                for key, value in products.items():
+                    for conf in value:
+                        if conf < 0.7:
+                            products.pop(key)
+
+            elif len(products) < total:
+                Server.set(
+                    "message",
+                    str({"code": "0", "detail": "Some products are undetectable."}),
+                )
+
             Server.set(
                 "products",
                 str([{"code": k, "quantity": v} for k, v in products.items()]),
             )
+            # Server.set("message", "Detect successfully with x% certainty.")
 
         # Release camera resources
         [
